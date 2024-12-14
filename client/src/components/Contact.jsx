a534981ab06dcc631a3a0a06bdd90287a770f1e0
Munesh Meena <110055748+Codecreatermunesh@users.noreply.github.com>
2024-12-14 22:02:09 +0530

    Update Contact.jsx

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import contactImage1 from "../Images/contact_munesh.jpeg";
+import contactImage1 from "../Images/contact_munesh_meena.jpeg";
 // import contactImage2 from "../Images/contactImage2.jpg";
 // import contactImage3 from "../Images/contactImage3.jpg";
 // import contactImage4 from "../Images/contactImage4.jpg";
